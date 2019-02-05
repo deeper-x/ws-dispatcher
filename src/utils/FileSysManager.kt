fb6b46d962c9c9191f15bc2124f83bf89f64e198
deeper-x <albertodeprezzo@gmail.com>
2019-02-06 00:46:05 +0100

    refactoring files/dirs

--- src/utils/FileSysManager.kt
+++ src/utils/FileSysManager.kt
@@ -5,7 +5,7 @@ import java.util.*
 
 class FileSysManager {
     private val _properties: Properties = Properties()
-    private val _propertyFile = System.getProperty("user.dir") + "/src/db/auth.properties"
+    private val _propertyFile = System.getProperty("user.dir") + "/assets/auth/auth.properties"
 
     fun getAuthProperty(): Properties {
         val fileReader = FileReader(this._propertyFile)
